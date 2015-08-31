3c9e8a0daf9e1a10cd5e95d67e6bc48ea600be95
giangd <giangd@rocketmail.com>
2015-08-30 23:44:08 -0700

    added tic tac toe

--- build-tmp/source/SampleAssignment.java
+++ build-tmp/source/SampleAssignment.java
@@ -1,13 +1,29 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SampleAssignment extends PApplet {
+
 int x;
 int y;
 int w;
 int h;
 int playerVal = -1; //the value of the current player--can only be -1 or 1, 0 represents an empty spot
-color[] colors = new color[9]; //stores the color of each square on the board
-color[] playerColors = new color[6]; //stores the value of 2 moused over (light) colors and 2 confirmed (dark) colors and 2 fill colors
+int[] colors = new int[9]; //stores the color of each square on the board
+int[] playerColors = new int[6]; //stores the value of 2 moused over (light) colors and 2 confirmed (dark) colors and 2 fill colors
 int[] board = new int[9]; //stores board data
 
-void setup() {
+public void setup() {
   size(600,600);
   background(136, 225, 247);
   playerColors[0] = color(255,0,0);  //moused over color for playerVal -1
@@ -22,7 +38,7 @@ void setup() {
   }
 }
 
-void draw() {
+public void draw() {
   
   x = -1; //numbers are wacky bc i wanted to make it look like 4 lines with 9 rects, so i made the nums like this
   y = -1;
@@ -30,7 +46,7 @@ void draw() {
   h = 201;
 
   for(int j=0; j<9; j++) {
-    //check this after you draw rect not before
+    rect(x,y,w,h);
     if (mouseX>x && mouseX <x+h && mouseY>y && mouseY<y+h && get(mouseX,mouseY) != playerColors[1] && get(mouseX,mouseY) != playerColors[4]) { 
       if (playerVal == -1) {
         fill(playerColors[0]); //preview color
@@ -52,7 +68,7 @@ void draw() {
     } else {
       fill(colors[j]);
     }
-    rect(x,y,w,h);
+    
     
     x += w;
     if (x > ((w-3)*3)) {
@@ -106,3 +122,12 @@ void draw() {
 //   line(311,285,353,296);
 //   }
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SampleAssignment" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
